0d4dd1058d3af737a35e32bb84ce884b6b1cac0b
Daniel Muelas <danmcode@gmail.com>
2025-08-12 14:19:38 -0500

    refactor(app & server): Refactor app y server code to follow the solid principles

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
 import express, { Application } from "express";
 import { Server } from "http";
+import { IApp } from "./interfaces/app.interface";
+import { IServer } from "./interfaces/server.interface";
+import { ServerConfig } from "./config/server.config";
+import { ExpressAppService } from "./services/express.app.service";
+import { HttpServerService } from "./services/http.server.service";
 
 export class App {
 
-    public express!: Application;
-    public httpServer!: Server;
+    private readonly appService!: IApp;
+    private readonly serverService: IServer;
+    private readonly config: ServerConfig;
 
-    public async init(): Promise<void> {
-        this.express = express();
-        this.httpServer = new Server(this.express);
+    constructor() {
+        this.config = new ServerConfig();
+        this.appService = new ExpressAppService();
+        this.serverService = new HttpServerService(
+            this.appService,
+            this.config
+        );
     }
+
+    public async start(): Promise<void> {
+        await this.serverService.start();
+    }
+
+    public async stop(): Promise<void> {
+        await this.serverService.stop();
+    }
+
+    public getPort(): number {
+        return this.serverService.getPort();
+    }
+
+
 }
